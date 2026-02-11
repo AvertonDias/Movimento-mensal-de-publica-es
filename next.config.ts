4e2185844af4335a0e04649711575795e1b87a40
Averton Silva Dias <verton3@gmail.com>
2026-02-11 12:31:49 +0000

    E esse link tem como usar?  https://cfp2.jw-cdn.org/a/87d17d/1/ir/nwt_T_

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cfp2.jw-cdn.org',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
