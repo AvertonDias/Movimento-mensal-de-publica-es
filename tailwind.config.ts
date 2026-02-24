affa0944d9ae666542020045c427b66c4e90bc3e
Averton Silva Dias <verton3@gmail.com>
2026-02-24 23:17:48 +0000

    mais rapido (_for element <Button>_)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -98,8 +98,8 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'pulse-slow': 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
-        'bounce-slow': 'bounce 3s infinite',
+        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        'bounce-slow': 'bounce 1.5s infinite',
         'rotate-phone': 'rotate-phone 4s ease-in-out infinite',
       },
     },
